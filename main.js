5c61f2792e5480feb7d14f210b9909db1d9a7095
Rachael Yoder <wayoder4@gmail.com>
2019-09-10 19:31:11 -0400

    added darken/lighten effect

--- main.js
+++ main.js
@@ -18,3 +18,15 @@ for (var i = 1; i <= 5; i++) {
   }
 }
 /* Wiring up the Darken/Lighten button */
+btn.onclick =function() {
+ var status = btn.getAttribute('class');
+ if (status === 'dark') {
+   btn.setAttribute('class', 'light');
+   btn.textContent = 'Lighten';
+   overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+ } else {
+   btn.setAttribute('class', 'dark');
+   btn.textContent = 'Darken';
+   overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+ }
+}
